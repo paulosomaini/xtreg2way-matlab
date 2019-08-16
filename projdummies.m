6960b292f4c5f42c061ab60a693bf042f9d2060c
Paulo Somaini <soma@stanford.edu>
2019-08-16 14:26:05 -0700

    Generalized inverse: a rank defective matrix means that we had to drop additional dummies. Find those automatically by taking the eigendecomposition of the matrix we want to invert

--- projdummies.m
+++ projdummies.m
@@ -15,8 +15,8 @@ HH=full(sum(DH,1)); HH=HH(1:end-1); DH=DH(:,1:end-1);
 invHH=sparse(1:struc.T-1,1:struc.T-1,HH.^(-1),struc.T-1,struc.T-1,struc.T-1);
 invDD=sparse(1:struc.N,1:struc.N,DD.^(-1),struc.N,struc.N,struc.N);
 useinv=1;
-if ~useinv;
-    if struc.N<struc.T;
+if ~useinv
+    if struc.N<struc.T
         struc.invA=diag(DD)-DH*invHH*DH';
         struc.B=-invA\DH*invHH;
         struc.C=invDD-invDD*DH/invA*DH'*invDD;
@@ -26,23 +26,29 @@ if ~useinv;
         struc.B=-A*DH*invHH;
     end
 else
-    if struc.N<struc.T;
-        struc.A=inv(diag(DD)-DH*invHH*DH');
+    if struc.N<struc.T
+        struc.A=ginv(diag(DD)-DH*invHH*DH');
         struc.C.invHH=invHH;
         struc.C.invHHDH=invHH*DH';
-        %struc.B=-struc.A*DH*invHH;
         struc.B=-struc.A*(struc.C.invHHDH)';
     else
-        struc.C=inv(diag(HH)-DH'*invDD*DH);
-        %invDD+invDD*DH*struc.C*DH'*invDD;
+        struc.C=ginv(diag(HH)-DH'*invDD*DH);
         struc.A.invDD=invDD;
         struc.A.invDDDH=invDD*DH;
-        %Balt=-(struc.A.invDD*DH*invHH+struc.A.invDDDH*struc.C*(struc.A.invDDDH'*DH*invHH));
-        %struc.B=-struc.A.invDD*DH*struc.C;
         struc.B=-struc.A.invDDDH*struc.C;
     end
         
 end
 
+function ginvA=ginv(A)
+    % generalized inverse: if matrix is rank-deficient it means that I have
+    % not eliminated enough dummy variables
+    [V,D] = eig(A);
+    D =diag(D); D(abs(D)<2e-12)=inf; 
+    ginvA = V*diag(D.^(-1))*V';
+   
+    
+    
+    
 
     
