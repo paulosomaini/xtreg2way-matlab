dcbfed038d04d859199c321b6794d193797d6e45
Lautaro Carrizo <lncarrizo1@gmail.com>
2020-06-15 17:54:18 -0300

    Improve Tables.m

--- Tables.m
+++ Tables.m
@@ -2,19 +2,23 @@
 clc
 %se==0
 diary table.txt
+disp('Xtreg2way')
 tic; xtreg2way(y,[x1 x2],hhid,tid,w,[],0,1); toc;
 diary off
 %se==1 (robust)
 diary table.txt
+disp('Xtreg2way-R')
 tic; xtreg2way(y,[x1 x2],hhid,tid,w,[],1,1); toc;
 diary off
 
 %one control
 %se==0
 diary table.txt
+disp('Xtreg2way-One C')
 tic; xtreg2way(y,x1,hhid,tid,w,[],0,1); toc;
 diary off
 %se==1
 diary table.txt
+disp('Xtreg2way-One C-R')
 tic; xtreg2way(y,x1,hhid,tid,w,[],1,1); toc;
 diary off
