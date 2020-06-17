49f3a6a04cd599d5bc8f06c416ab1a65ba15a7b8
Lautaro Carrizo <lncarrizo1@gmail.com>
2020-06-17 08:45:46 -0300

    Changes in Tables and Tables2000

--- Tables20000.m
+++ Tables20000.m
@@ -6,15 +6,15 @@ tic; xtreg2way(y,[x1 x2],hhid,tid,w,[],0,1); toc;
 diary off
 %se==1 (robust)
 diary table2000.txt
-tic; xtreg2way(y,[x1 x2],hhid,tid,w,[],1,1); toc;
+tic; xtreg2way(y,[x1 x2],hhid,tid,w,struc0,1,1); toc;
 diary off
 
 %one control
 %se==0
 diary table2000.txt
-tic; xtreg2way(y,x1,hhid,tid,w,[],0,1); toc;
+tic; xtreg2way(y,x1,hhid,tid,w,struc0,0,1); toc;
 diary off
 %se==1
 diary table2000.txt
-tic; xtreg2way(y,x1,hhid,tid,w,[],1,1); toc;
+tic; xtreg2way(y,x1,hhid,tid,w,struc0,1,1); toc;
 diary off
